839ac6c553c1088bf1f68d38048317de8888114d
강동욱 <ehddnr8563@gmail.com>
2024-01-06 11:17:35 +0900

    generateStaticParams 사용해 동적 라우팅한 페이지 중 미리 지정한 페이지 SSG로 만들어두기

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -3,6 +3,14 @@ type Props = {
     slug: string;
   };
 };
+
 export default function PantsPage({ params }: Props) {
   return <h1>{params.slug} 제품 설명 페이지</h1>;
+}
+
+export function generateStaticParams() {
+  const products = ['pants', 'skirt'];
+  return products.map((product) => ({
+    slug: product,
+  }));
 }
