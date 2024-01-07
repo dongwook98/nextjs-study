3728fbdd3c7ea7c7faa72efc3d0c81a0524050c8
강동욱 <ehddnr8563@gmail.com>
2024-01-08 02:23:49 +0900

    서버 컴포넌트

--- note/src/app/page.tsx
+++ note/src/app/page.tsx
@@ -1,9 +1,19 @@
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from './page.module.css';
+import os from 'os'; // 노드 APIs
+import Counter from '@/components/Counter';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <h1>홈페이지다!</h1>;
+  console.log('안녕!! - 서버'); // 서버에서만 실행됨
+  console.log(os.hostname()); // 서버 컴포넌트는 노드API 사용 가능
+
+  return (
+    <>
+      <h1>홈페이지다!</h1>
+      <Counter />
+    </>
+  );
 }
