67012cf33db1454f58a6571f0973ccf3a4d7305b
강동욱 <ehddnr8563@gmail.com>
2024-01-09 03:54:11 +0900

    Redirect

--- note/next.config.js
+++ note/next.config.js
@@ -11,6 +11,20 @@ const nextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/products/deleted_forever',
+        destination: '/products',
+        permanent: true, // 서치엔진한테 /products/deleted_forever가 영원히 삭제되었으니 /products를 캐시해도된다고 알려주는것
+      },
+      {
+        source: '/products/deleted_temp',
+        destination: '/products',
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
