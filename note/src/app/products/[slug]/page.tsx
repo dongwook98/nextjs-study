081ba655034410894c4b4386d3a8cedb8b9a458e
강동욱 <ehddnr8563@gmail.com>
2024-01-09 03:12:13 +0900

    제품 상세 페이지 Image 추가

--- note/src/app/products/[slug]/page.tsx
+++ note/src/app/products/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getProducts, getProduct } from '@/service/products';
 import { Metadata } from 'next';
+import Image from 'next/image';
 import { notFound } from 'next/navigation';
 
 // export const revalidate = 3;
@@ -30,7 +31,17 @@ export default async function ProductPage({ params: { slug } }: Props) {
     notFound();
   }
 
-  return <h1>{product.name} 제품 설명 페이지</h1>;
+  return (
+    <>
+      <h1>{product.name} 제품 설명 페이지</h1>
+      <Image
+        src={`/images/${product.image}`}
+        alt={product.name}
+        width='300'
+        height='300'
+      />
+    </>
+  );
 }
 
 /**
